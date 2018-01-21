a7a5235b12db3d5d55ce064d37862da042c283bd
Tim Kortsmit <tim@kortsmit.com>
2018-01-21 15:17:14 -0500

    Pull in markdown package and remove generated files

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'MSBA') }}</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/main.css') }}" rel="stylesheet">
