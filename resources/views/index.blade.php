0b8ff525e186f7dcd7ecd34e20ae657a968b6305
Tim Kortsmit <tim@kortsmit.com>
2018-01-21 21:55:40 -0500

    More styling

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,12 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'MSBA') }}</title>
 
-    <!-- Styles -->
     <link href="{{ asset('css/main.css') }}" rel="stylesheet">
 
 </head>
