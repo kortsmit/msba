da16ddc9468fa991540169ae87a5a55259ac6188
Tim Kortsmit <tim@kortsmit.com>
2018-01-21 19:12:32 -0500

    WIP: workshop notes

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -57,13 +57,13 @@ class PagesController extends Controller
 
 1. Introduction
 2. I attempted to prepare material that should take around two hours to 
-    get through, but of course I did not get to time how long it would 
-    take with a class following along. If we do not make it through everything 
-    or you're not quite able to keep up following along, I'll be making all 
-    of the notes for the workshop available to you. So, don't feel like 
-    you need to follow along the entire time.
+   get through, but of course I did not get to time how long it would 
+   take with a class following along. If we do not make it through everything 
+   or you're not quite able to keep up following along, I'll be making all 
+   of the notes for the workshop available to you. So, don't feel like 
+   you need to follow along the entire time.
 
-# Git
+# GIT
 
 ## Part 1 - Configuration
 
@@ -76,7 +76,7 @@ class PagesController extends Controller
     - `git config --global core.editor 'subl -n -w'`
     - `git config --list`
 
-## Part 1 - Basic Changes:
+## Part 2 - Basic Changes:
 
 For all of the basics steps in working with GitHub, we will quickly go 
 through and create a repository and work through a few example changes 
@@ -102,7 +102,7 @@ using if you work with GIT on a day to day basis.
 12. `git rm file3.txt` to remove the file
 13. `git commit -m 'Removing file 3'
 
-## Part 2 - History
+## Part 3 - Looking at Project History
 
 The power of Git starts becoming clear when you look at the history of files and your 
 project. Some quick example commands:
@@ -120,15 +120,32 @@ project. Some quick example commands:
     commit SHA in order to compare with the current commit. Or, you can pass two specific 
     commits to compare by passing the two SHAs separated by two periods. 
 
-## Part ? - Push to GitHub
+## Part 4 - Pushing to GitHub
 
 I would have covered this later, after talking about workflow, but I wanted to make sure 
 we pushed up to GitHub as getting your projects to GitHub is the ultimate goal of 
-this workshop.
+this workshop. If you have not already pushed your project to GitHub, feel free to
+switch to the project now but you will have to initialize it as a GIT repository and
+create an initial commit on the master branch.
 
-1. First, lets create a `readme.md` in the main 
+1. First, we'll go to GitHub and create a new repository
+2. Now, lets create a `readme.md` markdown file in the main project directory: 
+   `touch readme.md`
+3. Now edit it and create a brief description of the project we are working on:
+   `subl readme.md` (or another editor of choice)
+4. If you set up GitHub Desktop with your account, you've likely have SSH keys already
+   set up. My preference would be to use SSH and set 
+   `git remote set-url origin git@github.com:USERNAME/REPOSITORY.git`. However, if you have
+   not, you can also `git remote set-url origin https://github.com/USERNAME/REPOSITORY.git`.
+   You can verify if this is set correctly by running `git remote -v`
+5. Now, we'll have to set the upstream branch for our local one:
+   `git push --set-upstream origin master`. If you are using SSH keys, it'll push the code 
+   right up to the repository. If you're using HTTPS, you'll have to provide your
+   GitHub account credentials. If you have two factor authentication enabled, you'll
+   need to provide the current token instead of your account password.
+6. We'll come back to working with GitHub later
 
-## Part ? - Workflow
+## Part 5 - Workflow
 
 Think of GIT as a tree with the master branch being the main trunk. And workflow essentially 
 means to use branches in GIT that branch or separate your work from that trunk.
@@ -138,37 +155,63 @@ means to use branches in GIT that branch or separate your work from that trunk.
 2. `git branch` without parameters to view all local branches and `git branch --list -a` to 
     also view remote branches that are not cloned to your local repository
 3. `git branch branch-1` and `git branch branch-2` to create a few branches. These will be 
-    based on the current commit of the branch that you are on. Branches are great to keep work 
-    separate. A good rule of thumb is to branch early and to branch often.
+    based on the current commit of the branch that you are on, so be sure to know what your 
+    starting point is. Branches are great to keep work separate. A good rule of thumb is 
+    to branch early and to branch often. Branching in GIT is incredibly light weight, as
+    only changes are tracked.
 4. `git branch delete-this` and then you can delete the branch by using 
     `git branch -d delete-this`. I should note, you can not delete the 
     branch you are current on.
 5. `git log --oneline --decorate -a`: you can see that all of branches are currently at 
     the same exact commit. Here, `HEAD` indicates the most recent commit for the 
     branch you are currently on.
-6. 'git checkout branch-1' to check out another branch to do work on. This works for both 
-    local and remote branches.
+6. 'git checkout branch-1' to check out one of the branches we created earlier to do work 
+    on. This works for both local and remote branches.
+7. Another option would have been to run `git checkout -b branch-3`, which will create
+   branch-3 and check it out immediately. Most times, you are creating a new branch to 
+   do work on, so this is my personal preferred way of creating new branches.
 7. `date >> file2.txt` to make some quite edits
-8. `git add .` and `git commit -m \"Add date to file 2\"
+8. `git add .` and `git commit -m 'Add date to file 2'
 9. `git log --oneline --decorate -a`: you can now see that `branch-1` is ahead of the 
     other branches
 10. Now, say, we are happy with the changes we've made on `branch-1` and we'd like to 
     bring those changes into the `master` branch: `git checkout master` and then we'll 
     `git merge branch-1`
 11. `git log --oneline --decorate -a`: if we run git log again, you can now see that 
-    master and branch-1 are now at the same commit
+    master and branch-1 are now at the same commit, while branch-2 is now behind 
+    both branches.
+12. You can check out a previous commit using `git checkout SHA`. You will note that 
+    you might have a detached HEAD. All this means is that you are on a commit that 
+    does not have a branch associated with it. This option is great if you want to 
+    look around previous commit. For example, a new bug has popped up and you know 
+    that the functionality was working in a previous version. By checking out a 
+    previous commit, you'll be able to browse around and take a look at it.
 
-## Part ? - Sharing Work
+## Part 6 - Working with a team
 
-1. Branching strategies
-2. `master` is equivalent to what is currently in production. So, 
-3. `develop`
-4. Release branches. We name ours to the version on our road map they relate to. 
-    For example, we are currently working on `version-1.6`, while our master branch 
-    is tagged 
-5. Feature branches
-6. Bug fix branches
-3. Tag releases
+1. `git clone`
+2. Setting up a `.gitignore` file can be very useful if you're working with 
+   editors or have specific files that you would prefer to keep out of the repository.
+   For example, much of my work is done using PHPStorm as my code IDE. For each
+   project, PHPStorm keeps track of my personal preferences in a .idea directory.
+   Since I do not want to force my team to use those preferences, I'll ignore it.
+   Another reason is that you never want to include credentials or any other 
+   sensitive information on your repositories themselves. For our internal work, 
+   we keep specific environment files that hold these credentials, which are also 
+   ignored. You can use patterns or entire directories in this file. You can store 
+   them in different directories as well, you're not limited to a single file.
+3. You can use `git branch -a` to view a list of all branches, both local and remote.
+4. If you are working with multiple different people or from different computers,
+   there is a good chance that your local repository will get out of sync with the 
+   remote GitHub repository. You can use `git fetch` to download all of the new
+   work on the remote repository. You should note that `git fetch` only downloads
+   the work from the remote repository, it does not update your local repository.
+   It's a good way to determine if your local copy is out of sync. Keep in mind, 
+   even if you do out of sync work, GIT still protects you and basically forces
+   you to merge in the remote work before you can push your own work.
+4. To update your local repository, you can `git pull` changes to your local
+   repository. Effectively, this command runs both `git fetch` and `git merge`
+   together.
 
 # GitHub
 
@@ -177,9 +220,47 @@ means to use branches in GIT that branch or separate your work from that trunk.
 Pull requests can be used when working with a team as a form of review step to test 
 and review new features.
 
-1. 
-
 ## Issues
+
+Issues can be used for many things such as tasks, bug reports or just to discuss certain 
+topics or proposals for making changes to the project.
+
+1. Lets look at a few quick examples on the 
+   [kortsmit/msba](https://github.com/kortsmit/msba) repository.
+
+## Projects
+
+Projects allow you to organize the work you are doing in a form of simple project
+management, using GitHub's issues and pull requests. For example, you can see on the
+[kortsmit/msba](https://github.com/kortsmit/msba) repository's project page
+
+## Wiki
+
+On the wiki, you can use markdown to write documentation for the project. You can
+create new pages just by linking to them. For example, lets edit the home page I already
+created and add one more page. The path of the URL is case sensitive, and dashes are used
+to denote spaces in the new page's name.
+
+## Forking
+
+Basically, to fork a project just means that you are bringing a copy of it into your own
+GitHub account. From there, you can do anything you want with it. As an example, this is
+commonly used in Open Source development. Developers will fork a project to their own
+account, make the changes they would like to see in the original open source project and
+then create a pull request from their forked version back into the original. At that time,
+it is up to the project maintainer to decide if he wants to include the changes into the 
+project or not.
+
+## Adding a Project License
+
+To make it clear how you would like people to use your project, you will want to specify
+what type of license it has. The simplest way to add one would be to go to your master
+branch on GitHub, click on 'Create new file' and for the filename, type 'LICENSE.md'.
+An option on the right will appear that allows you to choose a license template. This
+is a great feature as it gives more information about what the license does and does 
+not allow. For example, I've applied the MIT license to the repository for this workshop 
+so that anyone can do anything they want with it, but there is no liability or warranty
+provided.
 
 # Advanced Workflow
 
@@ -187,14 +268,61 @@ While we do not have time to go over more advanced work, I wanted to show some v
 quick examples of what can be done when combining GIT, GitHub and other continuous 
 integration tools.
 
-## 
+1. Earlier, we talked about keeping the master branch stable and clean. For our my 
+   team's workflow, we protect both the master and develop branches on GitHub.
+2. By protecting it, it becomes impossible to directly merge or push to the branch.
+3. The only way to bring changes into protected GitHub branches is to use a pull 
+   request. Effectively, this starts a review process before any changes 
+4. As an example, let me show you why we might do that using a different project I
+   prepared for this workshop: [kortsmit/msba](https://github.com/kortsmit/msba)
+
+## Branching Workflows
+
+A great explanation of what we use at UniPro for our development can be accessed at 
+[Atlassian Tutorials](https://www.atlassian.com/git/tutorials/comparing-workflows/gitflow-workflow)
+
+# Options for Advanced Examples
+
+If time allows, here are a few examples that might be good to demonstrate as well:
+
+## Merge Conflicts
+
+If you are the only one working on a project, this might not happen too frequently.
+However, working as a team you will encounter this sometimes.
+
+1. 
+
+## Rebase
+
+Rebase can be used to bring a stale branch back up to date. For example, lets imagine
+you started a feature quite a while ago but then something else took priority. Now, you 
+are ready to get back to developing this new feature. GIT rebase will help you bring 
+this branch back up to date.
+
+You can also use GIT rebase to clean up a set of commits. For example, you might have
+made multiple commits while doing your work that might be able to be combined into a 
+single commit in order to keep your history clean.
+
+1. 
+
+## Cherry Pick
+
+GIT cherry-pick allows you to take a specific commit from another branch and add it 
+to your current one without merging in all other changes from that branch. You can also
+cherry pick multiple commits without having to pick them one at a time. I should note
+that most likely, you will not need this frequently. Personally, I've only used it on a 
+few instances, but it is good to be aware of when the need does arise.
+
+1. `git cherry-pick SHA` and pass it the commit's SHA that you are trying to add to 
+   your current branch.
+2. 
 
 # Questions and Comments
 
 I have business cards up front and my personal contact information will also be 
 available at [https://msba.kortsmit.com](https://msba.kortsmit.com). If anyone needs 
 any help setting up their repositories, has any questions or anything, please do not 
-hesitate to reach out. 
+hesitate to reach out.
         ";
     }
 }
